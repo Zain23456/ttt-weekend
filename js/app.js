d0118308bc22116559d5446942f5446141a22330
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-09-01 21:48:44 -0400

    game state initialization

--- js/app.js
+++ js/app.js
@@ -16,3 +16,13 @@ const messageEl = document.querySelector('#message')
 
 /*-------------------------------- Functions --------------------------------*/
 
+init()
+
+function init() {
+  board = ['','', '', '', '', '', '', '', '']
+  turn = 1 //represents player x player 0 is -1 * -1 to switch turns
+  winner = null
+  // value of 1 means x won, -1 means o won, 'T' is for tie
+  //render()
+  console.log('check')
+}
