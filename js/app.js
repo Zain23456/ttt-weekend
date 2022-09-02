bf8df5d0dd9e5eae6f7ec4eb6e6541947e9cde09
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-09-02 15:18:13 -0400

    render to user

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -9,7 +12,7 @@ let board, turn, winner
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.box')
 const messageEl = document.querySelector('#message')
-//console.log(messageEl);
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 
@@ -18,11 +21,38 @@ const messageEl = document.querySelector('#message')
 
 init()
 
+function render () {
+  board.forEach((square, index) => {
+    if (square === -1) {
+      squareEls[index].textContent = 'X'
+    }
+    else if (square === 1) {
+      squareEls[index].textContent = 'O'
+    }
+    if (winner === '') {
+      messageEl.textContent = `Your turn  Player ${turn}`
+  } else if (winner === 'T') {
+    messageEl.textContent = `It is a tie`
+  } else {
+    messageEl.textContent = `Player ${winner} wins!!`
+  }
+  })
+}
+    //} //renderWinner()
+    //render a message: if winner has a value of null us template literal inside a turnary to say which players turn it is
+  //renderMessage()
+
+    //if winner is equal to tie render a message
+    //else congradulatory message to the play that has one
+
+
 function init() {
-  board = ['','', '', '', '', '', '', '', '']
+  board = ['', '', '',
+          '', '', '',
+          '', '', '']
   turn = 1 //represents player x player 0 is -1 * -1 to switch turns
   winner = null
+  messageEl.textContent = 'Player X begins'
   // value of 1 means x won, -1 means o won, 'T' is for tie
-  //render()
-  console.log('check')
+  render()
 }
